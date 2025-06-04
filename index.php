8ec69bd20c09b3b2b42c74f23be278cf55f7e030
neizvedaniydag <62785983+neizvedaniydag@users.noreply.github.com>
2025-06-04 16:25:26 +0300

    Add Steam login skeleton

--- index.php
+++ index.php
@@ -1,74 +1,21 @@
-<html lang="en" class=" -webkit-"><head>
-
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="styles/modal.css">
-  <link rel="stylesheet" href="styles/style.css">
-  <title>ExchangeCases</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>ExchangeCases</title>
 </head>
-
-<body translate="no" class="container">
-
-      
-  <div class="row mb-4">
-    <div class="col text-center">
-      <h3>Обмен кейсов на скины</h3>
-      <a href="#" class="btn btn-lg btn-primary" data-toggle="modal" data-target="#largeModal" id="modal">Открыть список кейсов</a>
-    </div>
-  </div>
-  <h3>Ваши кейсы</h3>
-
-
-
-
-<!-- large modal -->
-<div class="modal fade" id="largeModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title" id="myModalLabel">Кейсы</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <h3>Выберете кейс для обмена</h3>
-      </div>
-      <div class="container" id="container">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<form action="" class="send">
-  <input type="text" id='case-send-input'>
-  <div id="case-send-img">
-</form>
-
-<div class="wrapper" id="wrapper"></div>
-<!--<img src="http://cdn.onlinewebfonts.com/svg/download_476397.png" alt="" class="change">-->
-
-
-
-    </div>
-  </div>
-</div> 
-
-      
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://unpkg.com/popper.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
-<script src="scripts/js-choose.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> 
-<div class="container">
-  
-
-
-   </div>
-
- 
-</body></html>
+<body>
+<h1>Обмен кейсов на скины</h1>
+<?php if(!isset($_SESSION['steamid'])): ?>
+    <a href="login.php">Войти через Steam</a>
+<?php else: ?>
+    <p>Вы вошли как <?php echo htmlspecialchars($_SESSION['steamid']); ?></p>
+    <a href="inventory.php">Открыть инвентарь</a> |
+    <a href="logout.php">Выйти</a>
+<?php endif; ?>
+</body>
+</html>
